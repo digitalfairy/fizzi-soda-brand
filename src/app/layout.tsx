8a1746a5abd661f54e3020350ced0eb335a415f5
Anita <jelicanita7@gmail.com>
2025-09-23 14:40:45 +0200

    Hero section and styling
    Styling with Tailwind CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,17 @@
+import localFont from "next/font/local";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+
+import "./app.css";
+import Header from "@/components/Header";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -7,8 +19,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={alpino.variable}>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header />
+        <main>
+          {children}
+        </main>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
