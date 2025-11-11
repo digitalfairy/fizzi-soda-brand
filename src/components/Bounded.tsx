e8453f30237ea6e7f8183eb543f3b829b4d01f61
Anita <jelicanita7@gmail.com>
2025-11-11 21:22:39 +0100

    Implemented generic props for Bounded.tsx

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,17 +1,16 @@
 import clsx from "clsx";
+import React from "react";
 
-type BoundedProps = {
-  as?: React.ElementType;
+export type BoundedProps<T extends React.ElementType> = React.ComponentPropsWithoutRef<T> & {
   className?: string;
-  children: React.ReactNode;
 };
 
-export const Bounded = ({
-  as: Comp = "section",
+export const Bounded = <T extends React.ElementType = "section">({
+  as: Comp = "section" as T, 
   className,
   children,
   ...restProps
-}: BoundedProps) => {
+}: BoundedProps<T>) => {
   return (
     <Comp
       className={clsx("px-4 first:pt-10 md:px-6", className)}
